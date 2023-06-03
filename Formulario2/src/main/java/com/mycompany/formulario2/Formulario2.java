f9dd947a05c08140ba556a30e1f89c7d49812031
Manuu <125753742+Manusm2712@users.noreply.github.com>
2023-06-03 16:40:18 -0500

    Add files via upload

---
+++ Formulario2/src/main/java/com/mycompany/formulario2/Formulario2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulario2;
+
+/**
+ *
+ * @author maria
+ */
+public class Formulario2 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
---
+++ Formulario2/src/main/java/com/mycompany/formulario2/Pitagoras.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.formulario2;
+
+/**
+ *
+ * @author maria
+ */
+public class Pitagoras {
+        public static double CalcularHipo (double ladoa, double ladob){
+        double hipotenusa;
+        hipotenusa = Math.sqrt(Math.pow(ladoa,2)+Math.pow(ladob,2));
+        return hipotenusa;
+     }
+}
